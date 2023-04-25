bfc260218eaa77d156352b2d1b92ee761c91fbce
Graciliano-Neto <116576105+Graciliano-Neto@users.noreply.github.com>
2023-04-25 15:23:29 -0300

    Add files via upload

---
+++ routes/desenRoutes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const desenController = require('../controllers/desenController');
+const router = Router();
+
+
+router.post('/', desenController.criarDesenvolvedor);
+
+router.get('/', desenController.verTodosDesenvolvedor);
+
+router.get('/:id', desenController.verDesenvolvedorPorId);
+
+router.patch('/:id', desenController.atualizarDesenvolvedor);
+
+router.delete('/:id', desenController.deletarDesenvolvedor);
+
+module.exports = router;
---
+++ routes/livrosRoutes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const livroscontroller = require('../controllers/livrosController');
+const router = Router();
+
+
+router.post('/', livroscontroller.criarLivros);
+
+router.get('/', livroscontroller.verTodosLivros);
+
+router.get('/:id', livroscontroller.verLivrosPorId);
+
+router.patch('/:id', livroscontroller.atualizarLivros);
+
+router.delete('/:id', livroscontroller.deletarLivros);
+
+module.exports = router;
